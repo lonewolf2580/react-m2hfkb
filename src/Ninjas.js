7056983471e9694e3f6e0b91e13a2e38f060995e
lonewolf2580 <linusngwee@yahoo.com>
2021-10-15 12:36:06 -0700

    1 file modified

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -3,18 +3,28 @@ import React, { Component } from 'react';
 const Ninjas = ({ ninjas }) => {
   // console.log(this.props);
   // const { ninjas } = props;
+  // const ninjaList = ninjas.map((ninja) => {
+  //   if (ninja.age > 20) {
+  //     return (
+  //       <div className="ninja" key={ninja.id}>
+  //         <div>Name: {ninja.name}</div>
+  //         <div>Age: {ninja.age}</div>
+  //         <div>Belt: {ninja.belt}</div>
+  //       </div>
+  //     );
+  //   } else {
+  //     return null;
+  //   }
+  // });
+
   const ninjaList = ninjas.map((ninja) => {
-    if (ninja.age > 20) {
-      return (
-        <div className="ninja" key={ninja.id}>
-          <div>Name: {ninja.name}</div>
-          <div>Age: {ninja.age}</div>
-          <div>Belt: {ninja.belt}</div>
-        </div>
-      );
-    } else {
-      return null;
-    }
+    ninja.age > 20 ? (
+      <div className="ninja" key={ninja.id}>
+        <div>Name: {ninja.name}</div>
+        <div>Age: {ninja.age}</div>
+        <div>Belt: {ninja.belt}</div>
+      </div>
+    ) : null;
   });
   return <div className="ninja-list">{ninjaList}</div>;
 };
