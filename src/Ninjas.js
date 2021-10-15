75877530b0333dfc69e9cf8ad1f80707aa0d62f9
lonewolf2580 <linusngwee@yahoo.com>
2021-10-15 12:44:18 -0700

    1 file modified

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -17,16 +17,19 @@ const Ninjas = ({ ninjas }) => {
   //   }
   // });
 
-  const ninjaList = ninjas.map((ninja) => {
-    ninja.age > 20 ? (
-      <div className="ninja" key={ninja.id}>
-        <div>Name: {ninja.name}</div>
-        <div>Age: {ninja.age}</div>
-        <div>Belt: {ninja.belt}</div>
-      </div>
-    ) : null;
-  });
-  return <div className="ninja-list">{ninjaList}</div>;
+  return (
+    <div className="ninja-list">
+      {ninjas.map((ninja) => {
+        return ninja.age > 20 ? (
+          <div className="ninja" key={ninja.id}>
+            <div>Name: {ninja.name}</div>
+            <div>Age: {ninja.age}</div>
+            <div>Belt: {ninja.belt}</div>
+          </div>
+        ) : null;
+      })}
+    </div>
+  );
 };
 
 export default Ninjas;
